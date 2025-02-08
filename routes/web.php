a66fdc405bd12cb17ebd648905c8f7e990c3f392
Ordna-dev <ordnajelaorerreug@gmail.com>
2025-02-08 13:01:20 -0600

    terminado

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/companies', [CompanyController::class, 'index']);
-Route::post('/tasks/create', [TaskController::class, 'store']);
+Route::prefix('api')->group(function () { // Opcional: versión de la API
+    Route::get('/companies', [CompanyController::class, 'index']);
+    Route::post('/tasks/create', [TaskController::class, 'store']);
+});
